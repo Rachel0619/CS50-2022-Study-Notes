a4b143c00e0c53ccdf9cf40cf07b5d62dd196787
Rachel0619 <115634891+Rachel0619@users.noreply.github.com>
2022-11-19 21:08:04 +0800

    Delete Lesson 2 C Programming directory

--- Lesson 2 C Programming/hello.c
+++
@@ -1,8 +0,0 @@
-#include<cs50.h> // h->header file
-#include<stdio.h> // standard input & output
-
-int main(void)
-{
-    string answer=get_string("What's your name?");
-    printf("hello, %s\n", answer);  //f means formatted %s is placeholder for string
-}
